63ba288a1efbe6cb6e99f175ace4c855c996f17c
damarisuwu1 <2109055@upy.edu.mx>
2022-01-28 09:52:17 -0600

    estructura general C

---
+++ unit1/generalStrucProgramC.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//* Global Variables *//;
+//Create a variable dataType nameVariable = anyData;
+//dataTypes in C/C++/C#/PHP
+int unEntero = 3;
+float unDecimal = 3.1416;
+char* name = "damarisuwu1";
+char unCharacter = 'M';
+///////////////////////////////
+
+
+/// La ejecucioon del programa//
+int main(){
+    printf("Hello Data B %s\n", name);
+
+    return 0;
+}
+///////////////////////////////
