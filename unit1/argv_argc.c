225e036d5808aa872ed84511c0bab50a9a330316
damarisuwu1 <2109055@upy.edu.mx>
2022-02-09 11:10:02 -0600

    argv lesson

---
+++ unit1/argv_argc.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char** argv){
+    printf("program executed: %s, number of inputs elements: %d", argv[0])
+    printf("Hola %s %s %s %s, bienvenido!\n", argv[1], argv[2], argv[3], argv[4]);
+    return 0;
+}
--- unit1/variablesGlobalLocal.c
+++ unit1/variablesGlobalLocal.c
@@ -8,7 +8,14 @@ char* date = "02-02-22T10:25:00z";
 /// La ejecucion del programa//
 int main(){
     date = "02-02-22T10:25:00z";
-    char* hours = "10:25:00z"; //Local:main
+    char* hours = "10:25:00z";
+    int hours = 22; //Local:main  %d %i
+    float pi = 3.1416; //%f
+    char character = 'c'; //%c
+    char* name = "damaris"; //%s
+    int *adress = &hours; //%p
+    hours = hours + 2;
+    printf(hours)
     return 0;
 }
 ///////////////////////////////
