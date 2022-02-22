8e66d499dac36c6d6a7ecb79c04e620df1f23b82
damarisuwu1 <2109055@upy.edu.mx>
2022-02-22 10:32:50 -0600

    add even odd

--- unit1/argumentos.c
+++ unit1/argumentos.c
@@ -2,7 +2,7 @@
 #include "utils.h"
 
 int main(){
-
+   
     int isGreater = isGreaterThan10(11);
     printf("the number is: %d", isGreater)
 
--- unit1/evenOdd.c
+++ unit1/evenOdd.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
 #include <stdbool.h>
-
-
+#include <stdlib.h>
 
 int main(int argc, char ** argv){
-    int n;
-    scanf ("number %d", &n);
-    bool notHasReminder = ( n % 2 == 0);
 
-    // if the number is perfectly divisible by 2
-    if (notHasReminder){ 
-        printf("The number %d is even \n", n);  
-    } else {
-        printf("The number %d is odd \n", n);
+    if (argc != 2){
+        printf ("Error program: U need to insert a number, e.g: ./%s argv[1]\n", argv[0]);
+        return 0;
     }
-    return 0;
+
+    bool isaNumber = (argv[1][0] >= 48 && argv[1][0] <= 57);
+
+    if (!isaNumber){
+        printf ("Error program: U need to insert a number, e.g: ./%s 8\n", argv[0]);
+        return 0;
+    }
+
+
+
+    int numberToTest = atoi(argv[1]);
+    int result = numberToTest % 2;
+    bool hasModule = ( result > 0 );
+
+    if (hasModule){ 
+       printf("impa\n");
+    } else {
+        printf("par\n");
+    
+    }
+    
 }
